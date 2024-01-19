32b9222e5687074ef5c977422fd8b0cbef1192f7
Mr.Lion <uchechukwuobiakor@gmail.com>
2024-01-19 20:47:32 +0100

    working on form sheet for creating collections

--- components/CreateCollectionBtn.tsx
+++ components/CreateCollectionBtn.tsx
@@ -1,6 +1,23 @@
+'use client'
+import { useState } from 'react'
+import { Button } from './ui/button'
+import CreateCollectionSheet from './CreateCollectionSheet'
 
 export default function CreateCollectionBtn() {
+  const [open, setOpen] = useState<boolean>(false)
+  const handleOpenChange = (open: boolean) => setOpen(open)
   return (
-    <div>CreateCollectionBtn</div>
+    <div className='w-full rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-[1px]'>
+      <Button
+        variant={'outline'}
+        className='dark:text-white w-full dark:bg-neutral-950 bg-white py-5'
+        onClick={() => setOpen(!open)}
+      >
+        <span className='bg-gradient-to-r from-red-500 to-orange-500 hover:to-orange-800 bg-clip-text text-transparent'>
+          Create Collection
+        </span>
+      </Button>
+      <CreateCollectionSheet open={open} handleOpenChange={handleOpenChange} />
+    </div>
   )
 }
--- components/CreateCollectionSheet.tsx
+++ components/CreateCollectionSheet.tsx
@@ -1,3 +1,29 @@
-export default function CreateCollectionSheet() {
-  return <div>CreateCollectionSheet</div>
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from './ui/sheet'
+
+type Props = {
+  open: boolean
+  handleOpenChange: (open: boolean) => void
+}
+export default function CreateCollectionSheet({
+  open,
+  handleOpenChange,
+}: Props) {
+  return (
+    <Sheet open={open} onOpenChange={handleOpenChange}>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>New Collection </SheetTitle>
+          <SheetDescription>
+            Collections are ways to group and organize your tasks
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  )
 }
